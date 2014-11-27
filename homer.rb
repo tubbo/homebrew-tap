db61076f0bd960c91c49d6c5762321130ef295f1
Tom Scott <tscott@telvue.com>
2014-11-27 02:23:48 -0500

    Update homer formula

--- homer.rb
+++ homer.rb
@@ -2,29 +2,25 @@ require "formula"
 
 class Homer < Formula
   homepage "https://github.com/tubbo/homer"
-  url "https://github.com/tubbo/homer/archive/master.tar.gz"
-  sha1 "aa79f2d98325b7688782ef836fc16a136eadbab1"
-  version "master"
+  url "https://github.com/tubbo/homer/archive/0.0.1.tar.gz"
+  sha1 "f7dd9557a96cfc93986b2785bf404b6a2ae872ae"
+  version "0.0.1"
   head "https://github.com/tubbo/homer.git"
+  #license 'MIT'
 
   depends_on 'chruby'
   depends_on 'ruby-install'
+  depends_on 'zsh'
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
 
-  def caveats; <<-EOS.undent
-    Thanks for installing Homer!
-
-    Run `homer init` to set up your home dir.
-    EOS
+  def caveats
+    "Run `homer init` to set up your home dir."
   end
 
   test do
-    assert system("ls /usr/local/lib/homer")
-    assert system("ls /usr/local/lib/homer.zsh")
-    assert system("ls /usr/local/lib/antigen.zsh")
-    assert system("ls /usr/local/share/homer")
+    system 'homer'
   end
 end
