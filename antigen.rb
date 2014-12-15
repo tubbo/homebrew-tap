937289bae43b95ab96fe4f35832a8b64eaec3888
Tom Scott <tscott@telvue.com>
2014-12-15 00:00:02 -0500

    Remove docs

--- antigen.rb
+++ antigen.rb
@@ -1,9 +1,5 @@
 require "formula"
 
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Antigen < Formula
   homepage "http://antigen.sharats.me/"
 
