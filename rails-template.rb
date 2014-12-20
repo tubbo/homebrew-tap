71e92046c9507a417d0254349348e2b109a78e0d
Tom Scott <tscott@telvue.com>
2014-12-20 00:00:28 -0500

    Update rails-template to 1.1

--- rails-template.rb
+++ rails-template.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class RailsTemplate < Formula
   homepage "https://github.com/tubbo/rails-template"
-  url "https://github.com/tubbo/rails-template/archive/v1.0.0.tar.gz"
-  sha1 "b10a66f1db2c44586ea05abaa18a35bbf63ec3a0"
+  url "https://github.com/tubbo/rails-template/archive/v1.1.0.tar.gz"
+  sha1 "f82ae8c05c5a09f39bf2564eef56ed9bbafc2ee4"
 
   def install
     system 'make'
   end
 
   test do
-    system "ls /usr/local/lib/rails-template", "ls /usr/local/share/rails-template"
+    system "ls /usr/local/share/rails-template/rails-template.rb", "ls /usr/local/share/rails-template/templates"
   end
 end
