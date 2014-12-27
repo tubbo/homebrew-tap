f9d46554f4d4fdd439b2df6a937f4349bb31d718
Tom Scott <tscott@telvue.com>
2014-12-27 16:47:41 -0500

    Update homer to v0.0.3

--- homer.rb
+++ homer.rb
@@ -1,24 +1,23 @@
-require "formula"
+require 'formula'
 
+# Homer is a home directory manager.
 class Homer < Formula
-  homepage "https://github.com/tubbo/homer"
-  url "https://github.com/tubbo/homer/archive/0.0.1.tar.gz"
-  sha1 "f7dd9557a96cfc93986b2785bf404b6a2ae872ae"
-  version "0.0.1"
-  head "https://github.com/tubbo/homer.git"
-  #license 'MIT'
+  homepage 'https://github.com/tubbo/homer'
+  url 'https://github.com/tubbo/homer/archive/v0.0.3.tar.gz'
+  sha1 'a6dc7ab9d74004c6db8f955c816d28bb773d79e0'
+  version '0.0.3'
+  head 'https://github.com/tubbo/homer.git'
 
-  depends_on 'chruby'
-  depends_on 'ruby-install'
   depends_on 'zsh'
   depends_on 'antigen'
+  depends_on 'keychain'
 
   def install
-    system "make", "install", "PREFIX=#{prefix}"
+    system 'make', 'install', "PREFIX=#{prefix}"
   end
 
   def caveats
-    "Run `homer init` to set up your home dir."
+    'Run `homer init` to set up your home dir.'
   end
 
   test do
