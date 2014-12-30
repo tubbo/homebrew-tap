3dc7c0056298c1cf0ffa5c4bc5b8ef148f3c6348
Tom Scott <tscott@telvue.com>
2014-12-30 16:57:08 -0500

    Add a HEAD to rails-template

--- rails-template.rb
+++ rails-template.rb
@@ -5,6 +5,7 @@ class RailsTemplate < Formula
   homepage 'https://github.com/tubbo/rails-template'
   url 'https://github.com/tubbo/rails-template/archive/v1.1.1.tar.gz'
   sha1 '4059a0bb460d4bb8608ef87a367f428cf22a1882'
+  head 'https://github.com/tubbo/rails-template.git'
 
   def install
     system 'make'
