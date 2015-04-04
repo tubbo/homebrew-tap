57730f21b0c536ae9767c464c3913bca5c72d73e
Tom Scott <tscott@weblinc.com>
2015-04-04 14:54:10 -0400

    Add caveat back and upgrade version

--- rails-template.rb
+++ rails-template.rb
@@ -3,12 +3,22 @@ require 'formula'
 # Installs my Rails application template.
 class RailsTemplate < Formula
   homepage 'https://github.com/tubbo/rails-template'
-  url 'https://github.com/tubbo/rails-template/archive/v1.1.1.tar.gz'
-  sha1 '4059a0bb460d4bb8608ef87a367f428cf22a1882'
+  url 'https://github.com/tubbo/rails-template/archive/v1.1.2.tar.gz'
+  sha1 'fa002063d54856c9f18b1495a9bc77639333c42e'
   head 'https://github.com/tubbo/rails-template.git'
 
   def install
     system 'make'
+  end
+
+  def caveats
+    %{
+      To finish installation, run the following command:
+
+        sudo ln -s /usr/local/etc/ $HOME/.railsrc
+
+      Then run `rails new` to start a new rails app!
+     }
   end
 
   test do
