3b7ef178d8e565904b7c2edc7fa5c9e26830ad48
Tom Scott <tscott@weblinc.com>
2015-05-03 22:55:02 -0400

    Upgrade to 0.0.4 and remove dependence on keychain

--- homer.rb
+++ homer.rb
@@ -3,14 +3,13 @@ require 'formula'
 # Homer is a home directory manager.
 class Homer < Formula
   homepage 'https://github.com/tubbo/homer'
-  url 'https://github.com/tubbo/homer/archive/v0.0.3.tar.gz'
-  sha1 '790893178a78cd897a79a1cfa90740a31ff74aff'
-  version '0.0.3'
+  url 'https://github.com/tubbo/homer/archive/v0.0.4.tar.gz'
+  sha1 '7e9e4dbc5131a32f46964aee02b5156990d710ea'
+  version '0.0.4'
   head 'https://github.com/tubbo/homer.git'
 
   depends_on 'zsh'
   depends_on 'antigen'
-  depends_on 'keychain'
 
   def install
     system 'make', 'install', "PREFIX=#{prefix}"
