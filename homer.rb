090439abd82bb3b6a92bf3a9ab460b52586f947f
Tom Scott <tscott@weblinc.com>
2015-05-30 18:56:02 -0400

    Update Homer to 0.0.6

--- homer.rb
+++ homer.rb
@@ -3,9 +3,9 @@ require 'formula'
 # Homer is a home directory manager.
 class Homer < Formula
   homepage 'https://github.com/tubbo/homer'
-  url 'https://github.com/tubbo/homer/archive/v0.0.5.tar.gz'
+  url 'https://github.com/tubbo/homer/archive/v0.0.6.tar.gz'
   sha1 '634ddca665b82e2560b03c6fb2a0af25e0f67abf'
-  version '0.0.5'
+  version '0.0.6'
   head 'https://github.com/tubbo/homer.git'
 
   depends_on 'zsh'
