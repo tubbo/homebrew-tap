52835ed822e40ff902a096d0ec77ea39b9f56063
Tom Scott <tscott@weblinc.com>
2015-07-12 21:12:55 -0400

    Update rails-template

--- rails-template.rb
+++ rails-template.rb
@@ -3,7 +3,7 @@ require 'formula'
 # Installs my Rails application template.
 class RailsTemplate < Formula
   homepage 'https://github.com/tubbo/rails-template'
-  url 'https://github.com/tubbo/rails-template/archive/v1.1.2.tar.gz'
+  url 'https://github.com/tubbo/rails-template/archive/v1.2.0.tar.gz'
   sha1 'd6f1e263b74c929cece5dd29babf7a2bb7114a6a'
   head 'https://github.com/tubbo/rails-template.git'
 
