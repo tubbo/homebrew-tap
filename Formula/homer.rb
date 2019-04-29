7957aeff1d8a6901a7de4cfda3f97b1800f646ac
Tom Scott <tscott@weblinc.com>
2019-04-29 13:55:08 -0400

    Fix SHA

--- Formula/homer.rb
+++ Formula/homer.rb
@@ -4,7 +4,7 @@ require 'formula'
 class Homer < Formula
   homepage 'https://github.com/tubbo/homer'
   url 'https://github.com/tubbo/homer/archive/v0.1.1.tar.gz'
-  sha256 '2fdcf7f2b8465f88f029a83e805ca1aec8ea5477'
+  sha256 'c10a6eece7546254f35c30825249c0e9a270f522b8e44a6bfa87a50f47c5fcbc'
   version '0.1.1'
   head 'https://github.com/tubbo/homer.git'
 
